dc6ad91f345b30f4931d5ad20e2bc32193e1aacf
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-10-21 21:51:04 +0530

    Add files via upload

---
+++ Queues/push queue_ex2.sv
@@ -0,0 +1,21 @@
+module queue_ex1;
+  int que1[$];
+  int que2[$];
+  int i;
+  
+  initial begin
+    que1 = '{1, 2, 3, 4, 5};
+    
+    $display("Initial queue:");
+    for (i = 0; i < que1.size(); i++)
+      $display("  que1[%0d] = %0d", i, que1[i]);
+    
+    
+    //Push_front operation
+    que1.push_front(7);
+    $display("\nAfter push front:");
+    for (i = 0; i < que1.size(); i++)
+      $display("  que1[%0d] = %0d", i, que1[i]);
+    
+  end
+endmodule
